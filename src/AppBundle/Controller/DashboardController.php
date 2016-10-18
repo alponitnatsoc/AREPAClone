8e3113e8440e2f0f231f2932a8d164956e67f97c
Andres Felipe Ramirez <alponitnatsnoc@gmail.com>
2016-10-17 21:22:15 -0500

    -added student, teacher and admin dashboards -added security configuration for pages -added lenguage support -changing constraints for security login and register -creating fixtures for person and user

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -3,17 +3,73 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction()
+    {
+        if($this->isGranted('IS_AUTHENTICATED_FULLY'))
+        {
+            return $this->redirectToRoute('dashboard',array(),307);
+        }else{
+            return $this->redirectToRoute('fos_user_security_login',array(),307);
+        }
+    }
+
+    public function dashboardAction(Request $request)
+    {
+        if($this->isGranted('IS_AUTHENTICATED_FULLY'))
+        {
+            if($this->isGranted('ROLE_ADMIN'))
+            {
+                return $this->redirectToRoute('admin_dasboard',array(),307);
+            }
+            elseif($this->isGranted('ROLE_TEACHER'))
+            {
+                return $this->redirectToRoute('teacher_dashboard',array(),307);
+            }
+            elseif($this->isGranted('ROLE_STUDENT'))
+            {
+                return $this->redirectToRoute('student_dashboard',array(),307);
+            }else{
+                return $this->render('@App/Dashboard/dashboard.html.twig');
+            }
+        }
+        else
+            {
+            return $this->redirectToRoute('fos_user_security_login',array(),307);
+        }
+    }
+
+    public function studentDashboardAction(Request $request)
+    {
+        if($this->isGranted('ROLE_STUDENT')){
+            return $this->render('@App/Student/student_dashboard.html.twig');
+        }else{
+            $this->createAccessDeniedException();
+        }
+        $this->redirectToRoute('dashboard',array(),307);
+    }
+
+    public function teacherDashboardAction(Request $request)
+    {
+        if($this->isGranted('ROLE_TEACHER')){
+            return $this->render('@App/Teacher/teacher_dashboard.html.twig');
+        }else{
+            $this->createAccessDeniedException();
+        }
+        $this->redirectToRoute('dashboard',array(),307);
+    }
+
+    public function adminDashboardAction(Request $request)
     {
         if($this->isGranted('ROLE_ADMIN')){
             return $this->render('@App/Admin/admin_dashboard.html.twig');
         }else{
-            return $this->createAccessDeniedException();
+            $this->createAccessDeniedException();
         }
-
+        $this->redirectToRoute('dashboard',array(),307);
     }
 }
